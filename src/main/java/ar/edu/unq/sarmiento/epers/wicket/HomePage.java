b2b2ef79c9b6c04a34f6bfc7bac8127b21199104
nahuel <nahuelmartinez827@gmail.com>
2018-10-29 20:51:50 -0300

    tratando de que ande la pagina homePage

--- src/main/java/ar/edu/unq/sarmiento/epers/wicket/HomePage.java
+++ src/main/java/ar/edu/unq/sarmiento/epers/wicket/HomePage.java
@@ -1,14 +1,22 @@
 package ar.edu.unq.sarmiento.epers.wicket;
 
+
 import org.apache.wicket.markup.html.WebPage;
+import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.model.PropertyModel;
 import org.apache.wicket.spring.injection.annot.SpringBean;
 
 public class HomePage extends WebPage{
+	private static final int ListItem = 0;
 	@SpringBean(name="controllerDeHomePage")
 	private ControllerDeHomePage controller;
+	
 	public HomePage() {
 		super();
-		
+		this.nombreDeDeveloper();
 	}
-
+	private void nombreDeDeveloper() {
+		this.add(new Label("nombreDeveloper", new PropertyModel<>(this.controller, "nombreDeveloper")));
+	}
+	
 }
