6fde1955c42101c11ac8c1d02567834281aee962
nahuel <nahuelmartinez827@gmail.com>
2018-10-30 20:20:32 -0300

    homePage Andando el nombre

--- src/main/java/ar/edu/unq/sarmiento/epers/wicket/ControllerDeHomePage.java
+++ src/main/java/ar/edu/unq/sarmiento/epers/wicket/ControllerDeHomePage.java
@@ -2,15 +2,20 @@ package ar.edu.unq.sarmiento.epers.wicket;
 
 import java.io.Serializable;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
+import ar.edu.unq.sarmiento.epers.home.AbstractHome;
+import ar.edu.unq.sarmiento.epers.home.Home;
 import ar.edu.unq.sarmiento.epers.model.Developer;
 @Component
 @Transactional
-public class ControllerDeHomePage implements Serializable{
+public class ControllerDeHomePage implements Serializable {
 	
 	private Developer desarrollador;
+	@Autowired
+	private Home<Developer> home;
 
 	public Developer getDesarrollador() {
 		return desarrollador;
@@ -19,8 +24,26 @@ public class ControllerDeHomePage implements Serializable{
 	public void setDesarrollador(Developer desarrollador) {
 		this.desarrollador = desarrollador;
 	}
-	public String nombreDeveloper(){
-		return desarrollador.getNombre();
+	public void buscarDeveloper(){
+		this.desarrollador = this.findByName(desarrollador.getNombre());
+	}
+	public String getNombre(){
+		return this.desarrollador.getNombre();
+	}
+
+
+	public Home<Developer> getHome() {
+		return home;
+	}
+
+	public void setHome(Home<Developer> home) {
+		this.home = home;
+	}
+
+	public Developer findByName(String name) {
+		return home.findByName(name);
+//		return this.getSession().createQuery("FROM Developer WHERE nombre = :name", Developer.class)
+//				.setParameter("name", name).getSingleResult();
 	}
 	
 	
--- src/main/java/ar/edu/unq/sarmiento/epers/wicket/HomePage.java
+++ src/main/java/ar/edu/unq/sarmiento/epers/wicket/HomePage.java
@@ -3,20 +3,30 @@ package ar.edu.unq.sarmiento.epers.wicket;
 
 import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.model.CompoundPropertyModel;
 import org.apache.wicket.model.PropertyModel;
 import org.apache.wicket.spring.injection.annot.SpringBean;
 
+import ar.edu.unq.sarmiento.epers.model.Developer;
+
 public class HomePage extends WebPage{
 	private static final int ListItem = 0;
+	
 	@SpringBean(name="controllerDeHomePage")
 	private ControllerDeHomePage controller;
+	private Developer controllerDeveloper=new Developer();
 	
-	public HomePage() {
+	public HomePage(){
+		this(new Developer());
+	}
+	@SuppressWarnings("serial")
+	public HomePage(Developer developer) {
 		super();
+		this.controller.setDesarrollador(developer);
 		this.nombreDeDeveloper();
 	}
 	private void nombreDeDeveloper() {
-		this.add(new Label("nombreDeveloper", new PropertyModel<>(this.controller, "nombreDeveloper")));
+		this.add(new Label("nombreDeveloper", new PropertyModel(controller, "nombre")));
 	}
 	
 }
