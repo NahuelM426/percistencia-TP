10f7afe354aaf792e61f9304bf2bc59df11abb18
Juan Cruz Santamaria <jcruzsantamaria@hotmail.com>
2018-10-30 20:22:46 -0300

    cambios hechos en home

--- src/main/java/ar/edu/unq/sarmiento/epers/wicket/ControllerDeHomePage.java
+++ src/main/java/ar/edu/unq/sarmiento/epers/wicket/ControllerDeHomePage.java
@@ -4,8 +4,24 @@ import java.io.Serializable;
 
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
+
+import ar.edu.unq.sarmiento.epers.model.Developer;
 @Component
 @Transactional
 public class ControllerDeHomePage implements Serializable{
+	
+	private Developer desarrollador;
 
+	public Developer getDesarrollador() {
+		return desarrollador;
+	}
+
+	public void setDesarrollador(Developer desarrollador) {
+		this.desarrollador = desarrollador;
+	}
+	public String nombreDeveloper(){
+		return desarrollador.getNombre();
+	}
+	
+	
 }
